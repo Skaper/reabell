dd572beccbf4819c85cb071f12b5468e26cf0139
Daniil Rostovsky <>
2021-11-11 01:42:04 +0400

    GravityFlipper
    Code clean up

--- Assets/Game/Scripts/GravityFlipper/GravityFlip.cs
+++ Assets/Game/Scripts/GravityFlipper/GravityFlip.cs
@@ -12,48 +12,47 @@ namespace GravityFlipper
 
         public bool isExitGravityZero = true;
 
-        private Rigidbody playerRigidbody;
+        
         private bool playerChengedGravity;
         
-        private void Start()
-        {
-            playerRigidbody = GameManager.Player.GetComponent<PlayerReferences>().rigRigidbody; 
-        }
 
         void OnTriggerStay(Collider col)
         {
-            if (col.CompareTag("Player/GravityCollider"))
+            if (!playerChengedGravity && col.CompareTag("Player/GravityCollider"))
             {
-                if (gravityTunnel && !gravityTunnel.isPlayerInTonnel)
+                if (gravityTunnel && gravityTunnel.isPlayerInTonnel)
                 {
-                    flipGravity();
-                    playerChengedGravity = true;
+                    return;
                 }
-            }else if (col.CompareTag("Block"))
+                FlipGravity(GameManager.Player.GetComponent<PlayerReferences>().transform);
+                playerChengedGravity = true;
+            }
+            else
             {
-                col.GetComponent<IGravityChanged>().OnGravityChanged(gravity);
+                var gravityChanged = col.GetComponent<IGravityChanged>();
+                gravityChanged?.OnGravityChanged(gravity);
             }
         }
 
       
-        private void flipGravity()
+        private void FlipGravity(Transform tr)
         {
             var targetGravity = Quaternion.Euler(angle) * Vector3.up;
-            RotateRigidbody(playerRigidbody.transform, targetGravity.normalized);
+            RotateRigidbody(tr, targetGravity.normalized);
         }
         
-        void RotateRigidbody(Transform _transform, Vector3 _targetDirection)
+        void RotateRigidbody(Transform tr, Vector3 targetDirection)
         {
             //Get rigidbody component of transform;
-            Rigidbody _rigidbody = _transform.GetComponent<Rigidbody>();
+            Rigidbody _rigidbody = tr.GetComponent<Rigidbody>();
 
-            _targetDirection.Normalize();
+            targetDirection.Normalize();
 
             //Calculate rotation difference;
-            Quaternion _rotationDifference = Quaternion.FromToRotation(_transform.up, _targetDirection);
+            Quaternion _rotationDifference = Quaternion.FromToRotation(tr.up, targetDirection);
 
             //Save start and end rotation;
-            Quaternion _endRotation = _rotationDifference * _transform.rotation;
+            Quaternion _endRotation = _rotationDifference * tr.rotation;
 
             _rigidbody.MoveRotation(_endRotation);
         }
